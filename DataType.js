afa35cb92ee6bb7e8acc406365da1e374569c17d
nahid-diu-cse <nahid.diu.cse@gmail.com>
2025-01-07 17:53:28 +0600

    first commit

---
+++ DataType.js
@@ -0,0 +1,17 @@
+const x = 'joy' //string
+const y = 10 //number
+const z = true //boolean        
+const a = {'name': 'joy'} //object  
+const b = [1,2,3] //array    
+const c = null //null
+const d = undefined //undefined
+const boolean = false //boolean 
+
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof boolean) 
